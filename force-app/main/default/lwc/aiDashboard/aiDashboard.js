af66bb1c9fd94f0b7ea3318bf43d63c2bc613ffa
Margish <margish.g@shrumtech.com>
2026-08-18 18:25:52 +0530

    Remove unnecessary space

--- force-app/main/default/lwc/aiDashboard/aiDashboard.js
+++ force-app/main/default/lwc/aiDashboard/aiDashboard.js
@@ -31,7 +31,6 @@ export default class AiDashboard extends LightningElement {
 	handleJsonChange(event) {
 		// alert('call handleJsonChange===');
 		this.jsonInput = event.target.value;
-
 	}
 
 	/*==============================================================
@@ -44,136 +43,98 @@ export default class AiDashboard extends LightningElement {
 
 		// Check empty input
 		if (!this.jsonInput) {
-
 			this.errorMessage = 'Please paste JSON';
-
 			return;
 		}
-
 		this.isLoading = true;
 
 		// Call Apex
-
 		parseDashboard({
 			jsonText: this.jsonInput
 		})
-
 			.then(result => {
-
 				console.log('Dashboard Result');
 				console.log(result);
-
 				/*
 				 * Save complete response
 				 */
-
 				this.dashboard = result;
-
 				/*
 				 * Prepare Widgets
 				 */
-
 				this.prepareWidgets();
-
 			})
 
 			.catch(error => {
-
 				console.error(error);
-
 				this.errorMessage =
 					error.body ?
 						error.body.message :
 						error.message;
-
 			})
-
 			.finally(() => {
-
 				this.isLoading = false;
-
 			});
-
 	}
 
 	/*==============================================================
 		Prepare Widgets
 		Purpose:
 		Add helper properties which HTML can directly use.
-
 		Instead of checking
-
 		widget.type == 'chart'
-
 		HTML simply uses
-
 		widget.isChart
 	==============================================================*/
 
 	prepareWidgets() {
-
 		if (!this.dashboard) {
-
 			return;
-
 		}
 
 		this.dashboard.widgets.forEach(widget => {
-
 			//--------------------------------------------------
 			// KPI
 			//--------------------------------------------------
-
 			widget.isKPI =
 				// widget.type === 'kpiGroup';
 				widget.type === 'kpi';
-
 			//--------------------------------------------------
 			// Chart
 			//--------------------------------------------------
-
 			widget.isChart =
 				widget.type === 'chart';
-
 			//--------------------------------------------------
 			// Table
 			//--------------------------------------------------
-
 			widget.isTable =
 				widget.type === 'table';
 
 			//--------------------------------------------------
 			// Timeline
 			//--------------------------------------------------
-
 			widget.isTimeline =
 				widget.type === 'timeline';
-
 			//--------------------------------------------------
 			// Progress
 			//--------------------------------------------------
 
 			widget.isProgress =
 				widget.type === 'progress';
-
 			//--------------------------------------------------
 			// List 
 			//--------------------------------------------------
-
 			widget.isList =
 				widget.type === 'list';
-
 		});
 
 		/*
 		 * Reassign object
 		 * So LWC refreshes UI
 		 */
-
 		this.dashboard = {
 			...this.dashboard
 		};
-
 	}
 
 	/*==============================================================
@@ -185,23 +146,16 @@ export default class AiDashboard extends LightningElement {
 	==============================================================*/
 
 	get showDashboard() {
-
 		return this.dashboard != null;
-
 	}
 
 	/*==============================================================
 		Show Error
-
 		HTML uses
-
 		<template if:true={showError}>
 	==============================================================*/
 
 	get showError() {
-
 		return this.errorMessage != null;
-
 	}
-
 }
